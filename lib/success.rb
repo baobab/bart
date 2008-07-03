978db0e0a36119486ca2cbe94a66e40b989a4d22
crazy <crazy@paiute.(none)>
2008-07-03 13:16:19 +0200

    Incremental commit - not tested

--- lib/success.rb
+++ lib/success.rb
@@ -1,11 +1,14 @@
+
 class Success
 
   def self.verify
-	  return unless self.active?
-    self.should_have_encounter_within_the_last_five_minutes	  
+	  return if self.sent_recent_alert?
     self.should_have_a_login_screen
     self.should_have_3_mongrels
     self.should_not_run_hot
+    if clinic_is_active?
+      self.should_have_encounter_within_the_last_five_minutes	  
+    end
 	end
 	
 	def self.reset(value = "")
@@ -15,46 +18,61 @@ class Success
 		property.save!
 	end
 
-  def self.active?
-    return false if last_alert && last_alert.to_time > 10.minutes.ago
-    start_time = GlobalProperty.find_by_property("success_start_hour").property_value.to_i rescue nil
-    end_time = GlobalProperty.find_by_property("success_end_hour").property_value.to_i rescue nil
-		return start_time.blank? || end_time.blank? || (Time.now.hour >= start_time && Time.now.hour < end_time)
+  def self.sent_recent_alert?
+    last_alert.to_time < 10.minutes.ago 
+    rescue
+      return false
   end
 
-	def self.start_hour=(value = "")
-    property = GlobalProperty.find_by_property("success_start_hour")
-		property ||= GlobalProperty.new(:property => "success_start_hour")
+  def self.clinic_is_active?
+    GlobalProperty.find_by_property("clinic_breaks").property_value.split(/, */).each{|break_range|
+      break_start, break_finish = break_range.split(/- */)
+      return false if Time.now.between?(break_start, break_finish)
+    }
+
+    GlobalProperty.find_by_property("clinic_hours").property_value.split(/, */).each{|clinic_hours_range|
+      clinic_hours_start, clinic_hours_finish = clinic_hours_range.split(/- */)
+      return false unless Time.now.between?(clinic_hours_start, clinic_hours_finish)
+    }
+    return true
+    rescue 
+      return true
+  end
+
+  def self.clinic_hours=(value = "7:30am-3:45pm")
+    Success.set_global_property("clinic_hours",value)
+  end
+
+  def self.clinic_breaks=(value = "12:30pm-1:00pm,1:00pm-1:15pm")
+    Success.set_global_property("clinic_breaks",value)
+  end
+
+  def self.set_global_property(property,value)
+    property = GlobalProperty.find_by_property(property)
+		property ||= GlobalProperty.new(:property => property)
 		property.property_value = value
 		property.save!
-	end
-	
-	def self.end_hour=(value = "")
-    property = GlobalProperty.find_by_property("success_end_hour")
-		property ||= GlobalProperty.new(:property => "success_end_hour")
-		property.property_value = value
-		property.save!
-	end
+  end
 	
 protected
 
   def self.should_have_encounter_within_the_last_five_minutes
   	last_encounter_time = Encounter.find(:first, :order => 'encounter_id DESC').date_created
-	  alert "Last encounter occurred more than five minutes ago (#{last_encounter_time})" if last_encounter_time < 5.minutes.ago
+	  self.alert "Last encounter occurred more than five minutes ago (#{last_encounter_time})" if last_encounter_time < 5.minutes.ago
 	end
 
   def self.should_have_a_login_screen
     login_screen = `lynx --dump localhost`
-    alert "No login screen available:\n #{login_screen}" unless login_screen.match(/Loading User Login/)
+    self.alert "No login screen available:\n #{login_screen}" unless login_screen.match(/Loading User Login/)
 	end
 
   def self.should_have_3_mongrels
-    alert "3 mongrels are not running:\npgrep mongrel\n#{`pgrep mongrel`}" unless `pgrep mongrel`.split(/\n/).length == 3 rescue false
+    self.alert "3 mongrels are not running:\npgrep mongrel\n#{`pgrep mongrel`}" unless `pgrep mongrel`.split(/\n/).length == 3 rescue false
   end
 
   def self.should_not_run_hot
     current_temp = `cat /proc/acpi/thermal_zone/THRM/temperature`.match(/\d+/).to_s.to_i rescue nil
-    alert "Machine is running hot: #{current_temp}" if current_temp.blank? || current_temp > 55
+    self.alert "Machine is running hot: #{current_temp}" if current_temp.blank? || current_temp > 55
   end
 
 private 
@@ -63,8 +81,21 @@ private
     Location.find(GlobalProperty.find_by_property("current_health_center_id").property_value).name rescue "Unknown location"
 	end
 	
-	def self.current_address
-  	`ifconfig | grep "192"`.match(/192\.168\.\d+\.\d+/)
+	def self.current_ip_address
+    # This code does not make an actual connection, but sets up
+    # a UDP connection and examines the route to figure out the IP
+    require 'socket'
+ 
+    orig, Socket.do_not_reverse_lookup = Socket.do_not_reverse_lookup, true  # turn off reverse DNS resolution temporarily  
+   
+    UDPSocket.open do |s|  
+      s.connect '64.233.187.99', 1 
+      s.addr.last  
+    end  
+  ensure  
+     Socket.do_not_reverse_lookup = orig  
+  end  
+
 	end
 
 	def self.last_alert
@@ -73,11 +104,23 @@ private
 		return nil
 	end
 
-  def self.alert(message)
-	  message = "#{self.current_location} (#{self.current_address}) was not successful on #{Time.now} with message:\n#{message}"
-    puts message
-    `echo "#{message}" | mail -s "Success failure" malawihackers@baobabhealth.org`
+  def self.alert(subject)
+    require 'net/smtp'
 		self.reset(DateTime.now.to_default_s)
-	end
+	  body = "#{self.current_location} (#{self.current_ip_address}) was not successful at #{Time.now} with message:\n#{message}"
+    sender = "success@baobabhealth.org"
+    receiver = "malawihackers@baobabhealth.org"
+
+    email_message = <<END_OF_MESSAGE
+From: Success <#{sender}>
+To: Support Team <#{to}>
+Subject: #{subject}
+#{message}
+END_OF_MESSAGE
+
+Net::SMTP.start('your.smtp.server', 25) do |smtp|
+  smtp.send_message email_message, sender, receiver
+end
+
 
 end
