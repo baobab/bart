8b27c9b287ce41b8af10d0a0ebe10ef7770e7245
Mwatha Bwanali <ace@lalanje.(none)>
2008-07-09 12:06:03 +0200

    writing specs "drug"

--- spec/models/drug_spec.rb
+++ spec/models/drug_spec.rb
@@ -26,5 +26,41 @@ describe Drug do
     drug = create_sample(Drug)
     drug.should be_valid
   end
-  
+
+  it "should find drug by name" do
+    Drug.find_by_name("Stavudine 6 Lamivudine 30 Nevirapine 50").should == drug(:drug_00056)
+  end
+
+  it "should display type of drug" do
+    drug(:drug_00056).type.to_s.should == "Stavudine Lamivudine Nevirapine Regimen"
+  end
+
+  it "should display if drug is an arv" do
+    drug(:drug_00056).arv?.should == true
+  end
+
+  it "should display drug abbreviation" do
+    drug(:drug_00056).to_abbreviation.split.to_s.should == "Oth:"
+  end  
+
+  it "should display drug short name" do
+    drug(:drug_00056).short_name.should == "d4T 3TC NVP"
+  end  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
