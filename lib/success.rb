d47521130e157b6ff73435ca5e0c4f0d031c30ab
crazy <crazy@paiute.(none)>
2008-07-10 17:54:57 +0200

    Worked on success.rb and wrote specs for it. Also speeded up spec_helper

--- lib/success.rb
+++ lib/success.rb
@@ -1,13 +1,26 @@
 
 class Success
 
+  @@sent_alert = false
+
+  def self.sent_alert
+    @@sent_alert
+  end
+
+  def self.sent_alert=(value)
+    @@sent_alert=value
+  end
+
   def self.verify
 	  return if self.sent_recent_alert?
     self.should_have_a_login_screen
     self.should_have_3_mongrels
     self.should_not_run_hot
+    self.should_have_free_memory
+    self.should_have_free_disk_space
+    self.should_have_more_than_ten_minutes_uptime
     if clinic_is_active?
-      self.should_have_encounter_within_the_last_five_minutes	  
+      self.should_have_recent_encounter
     end
 	end
 	
@@ -18,24 +31,26 @@ class Success
 		property.save!
 	end
 
-  def self.sent_recent_alert?
-    last_alert.to_time < 10.minutes.ago 
-    rescue
-      return false
+  def self.sent_recent_alert?(since = 10.minutes.ago)
+	  value = GlobalProperty.find_by_property("last_error_reported").property_value rescue nil
+	  last_alert = Time.parse(value) unless value.blank?
+    last_alert > since
+  rescue
+    return false
   end
 
-  def self.clinic_is_active?
+  def self.clinic_is_active?(current_time = Time.now)
     GlobalProperty.find_by_property("clinic_breaks").property_value.split(/, */).each{|break_range|
       break_start, break_finish = break_range.split(/- */)
-      return false if Time.now.between?(break_start, break_finish)
+      return false if current_time.between?(Time.parse(break_start), Time.parse(break_finish))
     }
 
     GlobalProperty.find_by_property("clinic_hours").property_value.split(/, */).each{|clinic_hours_range|
       clinic_hours_start, clinic_hours_finish = clinic_hours_range.split(/- */)
-      return false unless Time.now.between?(clinic_hours_start, clinic_hours_finish)
+      return false unless current_time.between?(Time.parse(clinic_hours_start), Time.parse(clinic_hours_finish))
     }
     return true
-    rescue 
+    rescue => exception
       return true
   end
 
@@ -47,22 +62,27 @@ class Success
     Success.set_global_property("clinic_breaks",value)
   end
 
+  def self.smtp_server=(value)
+    Success.set_global_property("smtp_server",value)
+  end
+
   def self.set_global_property(property,value)
-    property = GlobalProperty.find_by_property(property)
-		property ||= GlobalProperty.new(:property => property)
+    property = GlobalProperty.find_or_create_by_property(property)
+#		property ||= GlobalProperty.new(:property => property)
 		property.property_value = value
 		property.save!
   end
 	
 protected
 
-  def self.should_have_encounter_within_the_last_five_minutes
+  def self.should_have_recent_encounter(since = 5.minutes.ago)
   	last_encounter_time = Encounter.find(:first, :order => 'encounter_id DESC').date_created
-	  self.alert "Last encounter occurred more than five minutes ago (#{last_encounter_time})" if last_encounter_time < 5.minutes.ago
+	  self.alert "Last encounter occurred more than five minutes ago (#{last_encounter_time})" if last_encounter_time < since
 	end
 
   def self.should_have_a_login_screen
     login_screen = `lynx --dump localhost`
+    #login_screen = shell("lynx --dump localhost")
     self.alert "No login screen available:\n #{login_screen}" unless login_screen.match(/Loading User Login/)
 	end
 
@@ -71,27 +91,47 @@ protected
   end
 
   def self.should_not_run_hot
-    current_temp = `cat /proc/acpi/thermal_zone/THRM/temperature`.match(/\d+/).to_s.to_i rescue nil
+    current_temp = `cat /proc/acpi/thermal_zone/*/temperature | head -1`.match(/\d+/).to_s.to_i rescue nil
     self.alert "Machine is running hot: #{current_temp}" if current_temp.blank? || current_temp > 55
   end
 
   def self.should_have_free_memory
     mem_free = `cat /proc/meminfo | grep MemFree`
     mem_free_amount = mem_free.match(/\d+/)[0].to_i
-    alert "Machine is running out of memory: #{mem_free_amount}" if mem_free_amount < (256 * 1024)
+    alert "Machine is running out of memory: #{mem_free_amount}" if mem_free_amount < (256 * 1024) # 256 MB
   rescue
     alert "Could not check the free memory"      
   end
   
   def self.should_have_free_disk_space
-    disk_free = `df -h --block-size=1K /var/www | grep / `
+    disk_free = `df /var/www | grep / `
     disk_free_amount = disk_free.split[3].to_f
     alert "Machine is running out of disk space: #{disk_free_amount}KB free" if disk_free_amount < 1048576 # 1 Gig
   rescue
     alert "Could not check the free disk space"        
   end
 
+  def self.should_have_more_than_ten_minutes_uptime
+    uptime = `cat /proc/uptime`
+    uptime_seconds = uptime.split(/ +/)[0].to_f
+    alert "System was rebooted at #{Time.now - uptime_seconds}" if uptime_seconds/60 < 10
+  rescue 
+    alert "Could not check uptime"
+  end
+
+  def self.should_have_low_load_average
+    load_average = `cat /proc/loadavg`
+    fifteen_minute_load_average = load_average.split(/ +/)[2].to_f
+    alert "System has a high load average over the past 15 minutes: #{fifteen_minute_load_average}" if fifteen_minute_load_average > 1.5
+  rescue 
+    alert "Could not check load average"
+  end
+
 private 
+
+  def self.shell(string)
+    return `#{string}`
+  end
 
   def self.current_location
     Location.find(GlobalProperty.find_by_property("current_health_center_id").property_value).name rescue "Unknown location"
@@ -112,31 +152,39 @@ private
      Socket.do_not_reverse_lookup = orig  
   end  
 
-	end
-
-	def self.last_alert
-	  value = GlobalProperty.find_by_property("last_error_reported").property_value rescue nil
-	  return Time.parse(value) unless value.blank?
-		return nil
-	end
-
   def self.alert(subject)
+    @@sent_alert = true
+    #require 'smtp_tls'
     require 'net/smtp'
+
+    username = "baobabhealth"
+    password = "b40b4bhealth"
+
+
 		self.reset(DateTime.now.to_default_s)
-	  body = "#{self.current_location} (#{self.current_ip_address}) was not successful at #{Time.now} with message:\n#{message}"
+	  body = "#{self.current_location} (#{self.current_ip_address}) was not successful at #{Time.now} with message:\n#{subject}"
     sender = "success@baobabhealth.org"
     receiver = "malawihackers@baobabhealth.org"
 
     email_message = <<END_OF_MESSAGE
 From: Success <#{sender}>
-To: Support Team <#{to}>
+To: Support Team <#{receiver}>
 Subject: #{subject}
-#{message}
+
+#{body}
 END_OF_MESSAGE
 
-Net::SMTP.start('your.smtp.server', 25) do |smtp|
-  smtp.send_message email_message, sender, receiver
-end
+    smtp_server = self.global_property("smtp_server") || "localhost"
 
+    Net::SMTP.start(smtp_server, 25, 'localhost', "foo", "bar", :plain) do |smtp|
+      smtp.send_message email_message, sender, receiver
+    end
+
+  end
+
+  def self.global_property(property)
+    value = GlobalProperty.find_by_property(property)
+    value ? value.property_value : nil
+  end
 
 end
---
+++ spec/models/success_spec.rb
@@ -0,0 +1,230 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+require File.dirname(__FILE__) + '/encounter_spec'
+
+require 'net/smtp'
+
+# Need to do some crazy hacking stubbing stuff to test backtick results
+module Kernel
+  alias_method :backtick, :'`'
+
+  # $shell_result is used to stub the results of a backtick call
+  def `(cmd)
+    $shell_result
+  end
+end
+
+
+
+describe Success do
+
+  before(:each) do
+    Net::SMTP.stub!(:start).and_return(true)
+    Success.sent_alert = false
+  end
+
+  it "should verify the success of the site" do
+    $shell_result = '' # This will cause a problem and force an alert to be sent
+    Success.verify
+    Success.sent_alert.should == true
+  end
+
+  it "should not verify if an alert has recently been sent" do
+    Success.set_global_property("last_error_reported", 1.minute.ago)
+    Success.verify
+    Success.sent_alert.should == false
+  end
+
+  it "should reset last_error_reported property" do
+    Success.set_global_property("last_error_reported", 1.minute.ago)
+    Success.reset
+    GlobalProperty.find_by_property("last_error_reported").property_value.should be_blank
+  end
+
+  it "should check for recent alerts" do
+    Success.set_global_property("last_error_reported", 11.minute.ago)
+    Success.sent_recent_alert?.should == false
+    Success.set_global_property("last_error_reported", 1.minute.ago)
+    Success.sent_recent_alert?.should == true
+  end
+
+  it "should check if the clinic is active" do
+
+    Success.clinic_hours = nil
+    Success.clinic_breaks = nil
+    Success.clinic_is_active?.should == true
+
+    Success.clinic_hours=() # Use the default
+    Success.clinic_breaks=("10:30am-11:00am") 
+    Time.stub!(:now).and_return(Time.parse("10:45am"))
+    Success.clinic_is_active?.should == false
+
+    Success.clinic_hours=("8am-5pm") 
+    Success.clinic_breaks=("10:30am-11:00am") 
+    Time.stub!(:now).and_return(Time.parse("2:00pm"))
+    Success.clinic_is_active?.should == true
+
+    Time.stub!(:now).and_return(Time.parse("7:00pm"))
+    Success.clinic_is_active?.should == false
+  end
+
+  it "should set the clinic hours" do
+    Success.clinic_hours=("beer-thirty") 
+    GlobalProperty.find_by_property("clinic_hours").property_value.should == "beer-thirty"
+  end
+
+  it "should set the clinic breaks" do
+    Success.clinic_breaks=("beer-thirty") 
+    GlobalProperty.find_by_property("clinic_breaks").property_value.should == "beer-thirty"
+  end
+
+  it "should set the smtp server" do 
+    Success.smtp_server=("beer-thirty") 
+    GlobalProperty.find_by_property("smtp_server").property_value.should == "beer-thirty"
+  end
+
+  it "should be able to create and update global properties" do
+    Success.set_global_property("muppetballs", "fluffy")
+    GlobalProperty.find_by_property("muppetballs").property_value.should == "fluffy"
+    Success.set_global_property("muppetballs", "furry")
+    GlobalProperty.find_by_property("muppetballs").property_value.should == "furry"
+  end
+
+  it "should get the current location" do
+    Success.set_global_property("current_location_id", Location.current_location.id)
+    Success.current_location.should == Location.current_location.name
+  end
+
+  it "should get the current IP address" do
+    command_line_ip = backtick("ifconfig | grep 'inet ' | grep -v '127.0.0.1'").match(/inet addr:([^\s]*)/)[0].split(/:/).last
+    command_line_ip.should == Success.current_ip_address
+  end
+
+  it "should send email" do
+    Success.alert("My feet have smoking")
+  end
+
+end
+
+
+describe Success, "Tasks" do
+
+  before(:each) do
+    Net::SMTP.stub!(:start).and_return(true)
+    Success.sent_alert = false
+
+  end
+
+  it "should check for recent encounters and alert when there are none" do
+    Success.should_have_recent_encounter
+    Success.sent_alert.should == true
+  end
+
+  it "should check for recent encounters and not alert when there is one" do
+    encounter = create_sample(Encounter, :encounter_datetime => 1.minute.ago)
+    Success.should_have_recent_encounter
+    Success.sent_alert.should == false
+  end
+
+  it "should send alert when there is no login screen" do
+    $shell_result = "404"
+    Success.should_have_a_login_screen
+    Success.sent_alert.should == true
+  end
+
+  it "should not send alert when there is login screen" do
+    $shell_result = <<EOF
+                             Loading User Login
+
+   Username ______________________________
+   Password ______________________________
+   Submit
+
+                    Verifying your username and password
+
+                              Please wait......
+EOF
+    Success.should_have_a_login_screen
+    Success.sent_alert.should == false
+  end
+
+  it "should send alert when there are no running mongrels" do
+    $shell_result = ""
+    Success.should_have_3_mongrels
+    Success.sent_alert.should == true
+  end
+
+  it "should send alert when there are only two running mongrels" do
+    $shell_result = "12121\n3222"
+    Success.should_have_3_mongrels
+    Success.sent_alert.should == true
+  end
+
+  it "should not send alert when there are enough running mongrels" do
+    $shell_result = "12121\n3222\n55442"
+    Success.should_have_3_mongrels
+    Success.sent_alert.should == false
+  end
+
+  it "should send alert when machine is not hot" do
+    $shell_result = "temperature:             54 C\n"
+    Success.should_not_run_hot
+    Success.sent_alert.should == false
+  end
+
+  it "should send alert when machine is hot" do
+    $shell_result = "temperature:             66 C\n"
+    Success.should_not_run_hot
+    Success.sent_alert.should == true
+  end
+
+  it "should send alert when there is not enough free memory" do
+    $shell_result = "MemFree:         40736 kB\n" 
+    Success.should_have_free_memory
+    Success.sent_alert.should == true
+  end
+
+  it "should not send alert when there is enough free memory" do
+    $shell_result = "MemFree:         640736 kB\n" 
+    Success.should_have_free_memory
+    Success.sent_alert.should == false
+  end
+
+  it "should not send alert if free disk space is high" do
+    $shell_result =  "/dev/sda6             19228276   3688924  14562604  21% /var\n"
+    Success.should_have_free_disk_space
+    Success.sent_alert.should == false
+  end
+
+  it "should send alert if free disk space is low" do
+    $shell_result =  "/dev/sda6             19228276   3688924  1456  21% /var\n"
+    Success.should_have_free_disk_space
+    Success.sent_alert.should == true
+  end
+
+  it "should not send alert if more than one day uptime" do
+    $shell_result =  "244098.90 90781.20"
+    Success.should_have_more_than_ten_minutes_uptime
+    Success.sent_alert.should == false
+  end
+
+  it "should send alert if less than one day uptime" do
+    $shell_result =  "0.38 0.47 0.40 1/315 24294\n"
+    $shell_result =  "500 40"
+    Success.should_have_more_than_ten_minutes_uptime
+    Success.sent_alert.should == true
+  end
+
+  it "should send alert if load average is over 1.5" do
+    $shell_result =  "2.38 2.47 2.40 1/315 24294\n"
+    Success.should_have_low_load_average
+    Success.sent_alert.should == true
+  end
+
+  it "should not send alert if load average is less than 1.5" do
+    $shell_result =  "1.38 1.47 1.40 1/315 24294\n"
+    Success.should_have_low_load_average
+    Success.sent_alert.should == false
+  end
+
+
+end
