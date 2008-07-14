6f888d28cb020939b8acf5ab1fdb7473c73c5667
Mwatha Bwanali <ace@lalanje.(none)>
2008-07-14 12:19:55 +0200

    writing specs

--- spec/models/prescription_spec.rb
+++ spec/models/prescription_spec.rb
@@ -3,17 +3,21 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe Prescription do
   # You can move this to spec_helper.rb
 
-  sample({
-  })
+  before(:each) do
+    (@amount_remaining_from_last_visit, @dose_amount, @drug, @frequency, @time_period) = [2, 30, 'Stavudine Lamivudine', 'Morning', '1 month']
+    @prescription = Prescription.new(@drug, @frequency, @dose_amount, @time_period, @amount_remaining_from_last_visit)
+  end
 
   it "should be valid" do
-    #prescription = create_sample(Prescription)
-    (amount_remaining_from_last_visit, dose_amount, drug, frequency, time_period) = [2, 30, 'Stavudine Lamivudine', 'Morning', '1 month']
-    prescription = Prescription.new(drug, frequency, dose_amount, time_period, amount_remaining_from_last_visit)
-    prescription.dose_amount.should == dose_amount
-    prescription.drug.should == drug
-    prescription.frequency.should == frequency
-    prescription.time_period.should == time_period
+    prescription = @prescription
+    prescription.dose_amount.should == @dose_amount
+    prescription.drug.should == @drug
+    prescription.frequency.should == @frequency
+    prescription.time_period.should == @time_period
   end
+  
+  it "should display prescription as string"
+  
+  it "should display quantity"
   
 end
