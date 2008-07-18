308161348638c5dbcc4b902dc13da68c9bd7dc3f
Mwatha Bwanali <ace@lalanje.(none)>
2008-07-18 09:46:55 +0200

    working on a story "lab migration" - not finish yet

--- stories/all.rb
+++ stories/all.rb
@@ -1,3 +1,7 @@
+ENV["RAILS_ENV"] = "test"
+require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
+require 'spec/rails/story_adapter'
+
 dir = File.dirname(__FILE__)
 Dir[File.expand_path("#{dir}/**/*.rb")].uniq.each do |file|
   require file
--- stories/helper.rb
+++ stories/helper.rb
@@ -1,7 +1,3 @@
-ENV["RAILS_ENV"] = "test"
-require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
-require 'spec/rails/story_adapter'
-
 dir = File.dirname(__FILE__)
 Dir[File.expand_path("#{dir}/steps/*.rb")].uniq.each do |file|
   require file
