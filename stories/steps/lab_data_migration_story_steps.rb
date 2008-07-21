c8f2490a3850265bbcb5776d3259ff09d1fa6324
ice <mwatha@baobabhealth.org>
2008-07-21 11:12:47 +0200

    writing a story on "data synchronization" - 80% done!

--- stories/steps/lab_data_migration_story_steps.rb
+++ stories/steps/lab_data_migration_story_steps.rb
@@ -1,39 +1,31 @@
 steps_for(:lab_data_migration) do
   
-  Given "a username '$username'" do |username|
-    @username = username
-  end
-
-  Given "a password '$password'" do |password|
-    @password = password
-  end
-  
-  Given "a location '$location'" do |location|
-    @location = location
-  end
-
-  When "the user logs in with username and password" do
-    login_user(@username, @password, @location)
-  end
-
-  Then "should redirect to '$path'" do |path|
-    response.should redirect_to(path)
-  end
-
   Given "a logged in user" do
     login_user "mikmck","mike","701"
   end
 
-  Given "a task '$task'" do |task|
-    @task = task
+  Given "a task" do 
+    select_task("HIV Reception")
+  end
+  
+  When "the user clicks on finish" do
+    post "/patient/menu"
   end
 
-  When "the user submits the task" do
-    post "/user/change_activities", :user => { :activities => @task }
+  When "the user clicks on administration" do
+    get "/patient/admin_menu"
   end
-
-  Then "should redirect to '$path'" do |path|
+  
+  Then "should redirect to '$path'" do |path| 
     response.should redirect_to(path)
   end
 
+  When "the user clicks on Synchronize" do
+    get "/patient/synchronize_data"
+  end
+  
+  When "the user clicks on Sync Lab data" do
+    get "/"
+  end
+  
 end
