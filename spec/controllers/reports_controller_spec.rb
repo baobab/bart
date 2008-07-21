0806371871cf19eeebf5aad93765d4761ba98a43
Jeff Rafter <jeff@baobabhealth.org>
2008-07-21 11:02:28 +0200

    Adding specs for the reports controller

---
+++ spec/controllers/reports_controller_spec.rb
@@ -0,0 +1,14 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe ReportsController do
+
+  #Delete this example and add some real ones
+  it "should use ReportsController" do
+    controller.should be_an_instance_of(ReportsController)
+  end
+
+  it "should fail" do
+    1.should == 2
+  end
+
+end
