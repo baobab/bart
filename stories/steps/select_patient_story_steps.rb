91bdae2f5bb99fbc240b3744670ef9e7c7df6ce1
Soyapi Mumba <soyapim@gmail.com>
2008-07-31 15:12:00 +0200

    Added 2 patient exists scenarios

--- stories/steps/select_patient_story_steps.rb
+++ stories/steps/select_patient_story_steps.rb
@@ -2,18 +2,27 @@ steps_for(:select_patient) do
 
   Given "no current patient is selected" do
     login_user('mikmck', 'mike', '7001')
+    get '/patient/menu', {:no_auto_load_forms => true}
     #session[:patient_id] = nil
     @patient_id = nil
   end
 
+  Given "a patient '$barcode' is selected" do |barcode|
+    login_user('mikmck', 'mike', '7001')
+    post "/patient/set_patient/#{barcode}"
+  end
+
   When "the user scans '$wrong_barcode'" do |barcode|
-    get "/patient/set_patient/#{barcode}"
+    post "/patient/set_patient/#{barcode}"
   end
   
   Then "should redirect to '$path'" do |path|
     response.should redirect_to(path)
   end
 
-  Then "should display error '$error'"
+  Then "should display text '$text'" do |text|
+    follow_redirect!
+    response.should have_text(/#{text}/)
+  end
 
 end
--- stories/stories/select_patient_story.story
+++ stories/stories/select_patient_story.story
@@ -8,5 +8,16 @@ Story: User selecting a patient
     Given no current patient is selected
     When the user scans 'P1111'
     Then should redirect to '/patient/menu'
-    And should display error 'Sorry,Patient with id P1111 not found'
- 
+    And should display text 'Sorry,Patient with id P1111 not found'
+  
+  Scenario: Specified patient exists and no patient is already selected
+    Given no current patient is selected
+    When the user scans 'P170000000013'
+    Then should redirect to 'patient/patient_detail_summary'
+  
+  Scenario: Specified patient exists and a patient is already selected
+    Given a patient 'P170100176493' is selected
+    When the user scans 'P170000000013'
+    Then should redirect to 'patient/patient_detail_summary'
+    And should display text 'Andreas Jahn'
+
