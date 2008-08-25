6c38038c3b19621bdb3d3f92f5b92a79a070a347
Jeff Rafter <njero@smyrna.(none)>
2008-08-25 13:47:01 +0200

    sample'ing prescription_frequency view for spec purposes

--- spec/models/prescription_frequency_spec.rb
+++ spec/models/prescription_frequency_spec.rb
@@ -1,11 +1,18 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe PrescriptionFrequency do
+  set_fixture_class :prescription_frequency => PrescriptionFrequency
+
   fixtures :patient, :encounter, :encounter_type, :drug, :drug_ingredient, :drug_order, 
     :orders, :order_type, :concept, :concept_class, :concept_set, :obs
 
-  # If this fails then you need to migrate the views into your test database
-  it "should have the view" do
+	sample({
+		:frequency => "Once",
+		:frequency_days => 1
+	})
+
+  it "should have the table" do
+    rx = create_sample(PrescriptionFrequency)
     PrescriptionFrequency.find(:all).should_not be_empty
   end
   
