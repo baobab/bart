9d348472af99a0efc52b940ae55182315278b02b
ice <mwatha@baobabhealth.org>
2008-09-04 11:30:10 +0200

    fixing bug in spec_helper and new controller specs

--- spec/helpers/observation_helper_spec.rb
+++ spec/helpers/observation_helper_spec.rb
@@ -1,5 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
-
+=begin
 describe ObservationHelper do
   
   #Delete this example and add some real ones or delete this file
@@ -7,5 +7,5 @@ describe ObservationHelper do
     included_modules = (class << helper; self; end).send :included_modules
     included_modules.should include(ObservationHelper)
   end
-
 end
+=end
