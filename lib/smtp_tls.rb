8b85fef1a17f1f94d18ca952d77ff04d848284b1
Jeff Rafter <jeff@baobabhealth.org>
2008-09-23 10:35:06 -0700

    Update smtp tls for mixed ruby versions

--- lib/smtp_tls.rb
+++ lib/smtp_tls.rb
@@ -5,7 +5,9 @@ Net::SMTP.class_eval do
   private
   def do_start(helodomain, user, secret, authtype)
     raise IOError, 'SMTP session already started' if @started
-    check_auth_args user, secret, authtype if user or secret
+    if user or secret
+      check_auth_args user, secret
+    end
 
     sock = timeout(@open_timeout) { TCPSocket.open(@address, @port) }
     @socket = Net::InternetMessageIO.new(sock)
@@ -56,4 +58,3 @@ Net::SMTP.class_eval do
     getok('STARTTLS')
   end
 end
-
