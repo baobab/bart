22bffa7e9da8d7b855dfdbcd7991cedaece79257
Soyapi Mumba <soyapim@gmail.com>
2008-10-03 21:36:44 +0200

    Added weight_for_heights migration

---
+++ db/migrate/20081003193019_create_weight_for_heights.rb
@@ -0,0 +1,17 @@
+class CreateWeightForHeights < ActiveRecord::Migration
+  def self.up
+    drop_table :weight_for_heights
+    create_table :weight_for_heights  do |t|
+      t.float :supine_cm
+      t.float :median_weight_height
+      t.float :standard_low_weight_height
+      t.float :standard_high_weight_height
+      t.integer :sex, :limit => 6
+      t.string :height_sex, :limit => 5   
+    end
+  end
+
+  def self.down
+    drop :weight_for_heights
+  end
+end
