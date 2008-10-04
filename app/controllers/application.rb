2f68f8d29adab2f94024a9596606649c10ac417b
Soyapi Mumba <soyapim@gmail.com>
2008-10-04 19:27:42 +0200

    Fixed crash for deleted Patient Ids in cohort debugger; Show errors in production.log

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -86,17 +86,14 @@ EOF
     @message = exception.message
     @backtrace = exception.backtrace.join("\n") unless exception.nil?
     render :file => "#{RAILS_ROOT}/app/views/error/error.rhtml", :layout=> false, :status => 404
-  end #if RAILS_ENV == 'development' || RAILS_ENV == 'test'
+  end if RAILS_ENV == 'development' || RAILS_ENV == 'test'
 
-=begin
-  # we need users to see and report to developers the actual error message
   def rescue_action(exception)
     # do something based on exception
     @message = exception.message
     @backtrace = exception.backtrace.join("\n") unless exception.nil?
     render :file => "#{RAILS_ROOT}/app/views/error/error.rhtml", :layout=> false, :status => 404
   end if RAILS_ENV == 'production'
-=end
 
 #  def local_request?
 #    false
