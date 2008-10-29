a7783dd06435b7e36a9f3d8a4ac6aa29dd78504a
ice <mwatha@baobabhealth.org>
2008-10-29 10:01:58 +0200

    adding more checks "controller specs"

--- spec/controllers/encounter_type_controller_spec.rb
+++ spec/controllers/encounter_type_controller_spec.rb
@@ -15,11 +15,30 @@ describe EncounterTypeController do
     response.should be_success
   end
    
-  it "should create encounter type" do
-    post :update_params_filter, :encounter_type => {"encounter_type_id"=>"","name" => "Move file from dormant to active","description" =>"","creator" => User.current_user.id,"date_created" => Time.now}
+  it "should initialize a new encounter type" do
+    get :new
     response.should be_success
   end
+
+  it "should create encounter type" do
+    post :create, :encounter_type => {"name" => "Move file from dormant to active","description" =>"","creator" => User.current_user.id,"date_created" => Time.now}
+    response.should redirect_to("/encounter_type/list")
+  end
    
-    #response.should redirect_to("/patient/menu?")
+  it "should update encounter type" do
+    post :update, :id => encounter_type(:hiv_first_visit).id, :encounter_type => {"name" => "description"}
+    response.should redirect_to("/encounter_type/list")
+  end
+   
+  it "should destroy encounter type" do
+    post :destroy, :id => encounter_type(:hiv_first_visit).id
+    response.should redirect_to("/encounter_type/list")
+  end
+   
+  it "should cancel an encounter type" do
+    get :cancel
+    response.should redirect_to("/encounter_type/list")
+  end
+   
 
 end
