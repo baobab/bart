026ca6cca0d9206b8d47d52a573fc2db874ab0fa
dave gamphani phiri <gamphani@kafyontha.(none)>
2009-01-05 16:37:23 +0200

    finalising cohort report links

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -531,18 +531,29 @@ class ReportsController < ApplicationController
       cohort = Reports::CohortByRegistrationDate.new(start_date.to_date, end_date.to_date)
       @key = params[:id].to_sym
       @field = params[:field]
+      
+      dead_patients = cohort.patients_with_outcomes('Died')
+      transfer_out_patients = cohort.patients_with_outcomes('Transfer out,Transfer Out(With Transfer Note),Transfer Out(Without Transfer Note)'.split(','))
+      stopped_patients = cohort.patients_with_outcomes('ART stop')
+      deffaulted_patients = cohort.patients_with_outcomes('Defaulter')
+
       case params[:id]
       when 'occupations'
+        if @field == 'soldier/police'
+          @field = 'solder,police'
+          @patients = cohort.patients_with_occupations(@field.gsub("/", ' ').split(','))
+        else
           @patients = cohort.patients_with_occupations(@field.split(','))
+        end
+          @field = params[:field]
+      when 'regimen_types'
+          @patients = cohort.find_all_patient_art_regimens(@field.gsub('_',' '))
+          @patients = @patients - (dead_patients + transfer_out_patients + stopped_patients + deffaulted_patients)
       when 'outcome'
           if @field == 'transferred_out'
              @field = 'Transfer out,Transfer Out(With Transfer Note),Transfer Out(Without Transfer Note)'
              @patients = cohort.patients_with_outcomes(@field.gsub('_', ' ').split(','))
           elsif @field == 'alive_on_art'
-            dead_patients = cohort.patients_with_outcomes('Died')
-            transfer_out_patients = cohort.patients_with_outcomes('Transfer out,Transfer Out(With Transfer Note),Transfer Out(Without Transfer Note)'.split(','))
-            stopped_patients = cohort.patients_with_outcomes('ART stop')
-            deffaulted_patients = cohort.patients_with_outcomes('Defaulter')
             on_art_patients = cohort.patients_with_outcomes('On ART')
             @patients = on_art_patients - (dead_patients + transfer_out_patients + stopped_patients + deffaulted_patients)
           else
@@ -557,7 +568,12 @@ class ReportsController < ApplicationController
           names_to_ids = {'at_work_or_school' => Concept.find_by_name('Is at work/school').id}
           @patients = cohort.find_patients_with_last_observation([names_to_ids[@field]])
         elsif @field == 'side_effects_patients'
-          names_to_ids = {'side_effects_patients' => Concept.find_by_name('Skin rash').id}
+          concept_ids = []
+          side_effects = ['Skin rash','Hepatitis','Peripheral neuropathy']
+          side_effects.each{|name|
+            concept_ids << Concept.find_by_name(name).id
+          }
+          names_to_ids = {'side_effects_patients' => concept_ids}
           @patients = cohort.find_patients_with_last_observation([names_to_ids[@field]])
         elsif @field == 'on_1st_line_with_pill_count_adults'
           @patients = cohort.find_all_adults_on_first_line_with_pill_count
