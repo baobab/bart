ecd6f2542627ab05ccd75293f3ab672848c397da
Wilson Bilkovich <wilson@supremetyrant.com>
2009-03-01 17:46:43 -0500

    Run the base schema migration in dev mode as well.
    Fixes rake db:test:prepare

--- db/migrate/20080405175950_load_base_legacy_schema.rb
+++ db/migrate/20080405175950_load_base_legacy_schema.rb
@@ -1,10 +1,8 @@
 class LoadBaseLegacySchema < ActiveRecord::Migration
   def self.up
-    # TODO: figure out why this is only run in test environment
-    return unless RAILS_ENV == 'test'
-    database = ActiveRecord::Base.connection.instance_eval('@config')[:database]
-    user = ActiveRecord::Base.connection.instance_eval('@config')[:username]
-    password = ActiveRecord::Base.connection.instance_eval('@config')[:password]
+    database = ActiveRecord::Base.connection.instance_variable_get('@config')[:database]
+    user = ActiveRecord::Base.connection.instance_variable_get('@config')[:username]
+    password = ActiveRecord::Base.connection.instance_variable_get('@config')[:password]
     file = File.expand_path(RAILS_ROOT + "/db/schema.sql")
 
     `mysql -u #{user} #{"--password=#{password}" if password} #{database} < #{file}`
