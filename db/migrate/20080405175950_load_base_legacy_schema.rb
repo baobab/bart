85051a89e2a50de2dc654e48a2ea245c86d4ca5d
Jacqui Maher and Wilson Bilkovich <dev+jacqui+wilson@hashrocket.com>
2009-03-11 13:50:27 -0400

    Create initial system user before adding foreign key constraint that prevents it

--- db/migrate/20080405175950_load_base_legacy_schema.rb
+++ db/migrate/20080405175950_load_base_legacy_schema.rb
@@ -6,8 +6,18 @@ class LoadBaseLegacySchema < ActiveRecord::Migration
     file = File.expand_path(RAILS_ROOT + "/db/schema.sql")
 
     `mysql -u #{user} #{"--password=#{password}" if password} #{database} < #{file}`
+
+    execute "ALTER TABLE users DROP FOREIGN KEY `user_creator`"
+    # create first user
+    salt = User.random_string(10)
+    password = User.encrypt(User.random_string(8), salt)
+
+    User.create!(:salt => salt, :password => password, :creator => '1', :first_name => 'Baobab', :last_name => 'Admin', :system_id => 'Baobab Admin', :username => 'baobadmin')
+
+    execute "alter table users add CONSTRAINT `user_creator` FOREIGN KEY (`creator`) REFERENCES `users` (`user_id`)"
   end
 
   def self.down
+    ActiveRecord::Base.connection.tables.each{ |t| drop_table t.to_sym unless ["schema_migrations", "migrations_info"].include? t }
   end
 end
