d9c039837ac2a0f13d4656c9d3b5b03651e596b5
Mwatha Bwanali <ace@mombo.(none)>
2009-03-13 12:34:50 +0200

    cleaning up code - printing

--- app/controllers/label_printing_controller.rb
+++ app/controllers/label_printing_controller.rb
@@ -14,20 +14,12 @@ class LabelPrintingController < ApplicationController
      user_real_name=user.name
      user_role = user.roles.first.role
      
-  string_label=<<EOF
-""
-N
-q776
-Q329,026
-A40,30,0,2,2,2,N,"#{user_real_name.delete("'")}"
-A40,80,0,2,2,2,N,"#{user_role}"
-B40,130,0,1,5,15,120,B,"#{user_name}"
-P1
-N\\f
+     label = ZebraPrinter::StandardLabel.new
+     label.draw_text("#{user_real_name.delete("'")}", 40, 30, 0, 2, 2, 2, false)
+     label.draw_text("Quantity: #{user_role}", 40, 80, 0, 2, 2, 2,false)
+     label.draw_barcode(40, 130, 0, 1, 5, 15, 120,true, "#{user_name}")
 
-EOF
-
-      send_data(string_label,:type=>"application/label; charset=utf-8", :stream=> false, :filename=>"#{user.id}#{rand(10000)}.lbl", :disposition => "inline")
+     send_data(label.print(1),:type=>"application/label; charset=utf-8", :stream=> false, :filename=>"#{user.id}#{rand(10000)}.lbl", :disposition => "inline")
   end
 
   
@@ -48,15 +40,15 @@ EOF
 
      if drug_string_length <= 27 
        label = ZebraPrinter::StandardLabel.new
-       label.draw_barcode(40, 130, 0, 1, 5, 15, 120,false, "#{drug_barcode}")
        label.draw_text("#{drug_name}", 40, 30, 0, 2, 2, 2, false)
        label.draw_text("Quantity: #{drug_quantity}", 40, 80, 0, 2, 2, 2,false)
+       label.draw_barcode(40, 130, 0, 1, 5, 15, 120,true, "#{drug_barcode}")
      else  
        label = ZebraPrinter::StandardLabel.new
        label.draw_text("#{drug_name1}", 40, 30, 0, 2, 2, 2, false)
        label.draw_text("#{drug_name2}", 40, 80, 0, 2, 2, 2, false)
        label.draw_text("Quantity: #{drug_quantity}", 40, 130, 0, 2, 2, 2,false)
-       label.draw_barcode(40, 180, 0, 1, 5, 15, 120,false, "#{drug_barcode}")
+       label.draw_barcode(40, 180, 0, 1, 5, 15, 100,true, "#{drug_barcode}")
      end
 
       send_data(label.print(1),:type=>"application/label; charset=utf-8", :stream=> false, :filename=>"#{drug_barcode}.lbl", :disposition => "inline")
