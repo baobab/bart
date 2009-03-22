59ee16ad3d6e19ccb184ff960c2cecfa031e3799
Jacqui Maher <jacqui@brighter.net>
2009-03-22 12:28:26 -0500

    Refactored FieldType hash cache initialization & fixed failing spec examples

--- app/models/field_type.rb
+++ app/models/field_type.rb
@@ -1,37 +1,37 @@
 class FieldType < OpenMRS
   set_table_name "field_type"
+  set_primary_key "field_type_id"
   has_many :fields, :foreign_key => :field_type
   belongs_to :user, :foreign_key => :user_id
-#field_type_id
-  set_primary_key "field_type_id"
 
-  @@field_type_hash_by_name = Hash.new
-  @@field_type_hash_by_id = Hash.new
-  self.find(:all).each{|field_type|
-    @@field_type_hash_by_name[field_type.name.downcase] = field_type
-    @@field_type_hash_by_id[field_type.id] = field_type
-  }
+  class << self
+    def build_hash_cache
+      @@field_type_hash_by_name = Hash.new
+      @@field_type_hash_by_id = Hash.new
+      find(:all).each do |field_type|
+        @@field_type_hash_by_name[field_type.name.downcase] = field_type
+        @@field_type_hash_by_id[field_type.id] = field_type
+      end
+    end
 
-# Use the cache hash to get these fast
-  def self.find_from_ids(args, options)
-    super if args.length > 1 and return
-    return @@field_type_hash_by_id[args.first] || super
+    # Use the cache hash to get these fast
+    def find_from_ids(args, options = {})
+      build_hash_cache if @@field_type_hash_by_id.blank?
+
+      args.flatten!
+      results = args.map{|id| @@field_type_hash_by_id[id]}.compact
+      return results.first if !results.blank? && args.size == 1
+      return results if !results.blank? && results.size == args.size
+      super
+    end
+
+    def find_by_name(field_type_name)
+      build_hash_cache if @@field_type_hash_by_name.blank?
+      return @@field_type_hash_by_name[field_type_name.downcase] || super
+    end
   end
-  
-  def self.find_by_name(field_type_name)
-    return @@field_type_hash_by_name[field_type_name.downcase] || super
-  end
-  
+
+  build_hash_cache
+
 end
 
-
-### Original SQL Definition for field_type #### 
-#   `field_type_id` int(11) NOT NULL auto_increment,
-#   `name` varchar(50) default NULL,
-#   `description` longtext,
-#   `is_set` tinyint(1) NOT NULL default '0',
-#   `creator` int(11) NOT NULL default '0',
-#   `date_created` datetime NOT NULL default '0000-00-00 00:00:00',
-#   PRIMARY KEY  (`field_type_id`),
-#   KEY `user_who_created_field_type` (`creator`),
-#   CONSTRAINT `user_who_created_field_type` FOREIGN KEY (`creator`) REFERENCES `users` (`user_id`)
--- spec/models/field_type_spec.rb
+++ spec/models/field_type_spec.rb
@@ -15,17 +15,17 @@ describe FieldType do
     field_type = create_sample(FieldType)
     field_type.should be_valid
   end
-  
+
   it "should find from ids" do
-    FieldType.find_from_ids([11], nil).should == field_type(:field_type_00011)
+    FieldType.find_from_ids([11]).should == field_type(:field_type_00011)
     field_type = FieldType.find(11)
     field_type.id = 99
     field_type.save
-    FieldType.find(11).id.should == 99  #use cached one
-    FieldType.find([11,10]).map(&:id).should == [10,11] # don't why it's still finding 11
+    FieldType.find(11).id.should == 99  # use cached one
+    FieldType.find([11,10]).map(&:id).should == [99,10]
   end
 
-  it "should find by name" do 
+  it "should find by name" do
     FieldType.find_by_name("select").should == field_type(:field_type_00002)
   end
 
