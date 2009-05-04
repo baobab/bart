e37b0d05e57e449e9090393306b690758f66cc2d
Mwatha Bwanali <ace@mombo.(none)>
2009-05-04 11:47:52 +0200

    adding weight to pat demog print out

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3424,7 +3424,7 @@ EOF
      label2.draw_text("#{second_line_drugs_date}",380,260,0,2,1,1,false)
 
      label2.draw_text("HEIGHT: #{self.initial_height.to_s + ' (cm)' rescue nil}",600,60,0,2,1,1,false)
-     label2.draw_text("WEIGHT: #{self.initial_weight + ' (kg)' rescue nil}",600,100,0,2,1,1,false)
+     label2.draw_text("WEIGHT: #{self.initial_weight.to_s + ' (kg)' rescue nil}",600,100,0,2,1,1,false)
      label2.draw_text("Init Age: #{self.age_at_initiation}",600,140,0,2,1,1,false)
      label2.draw_text("#{first_line_drugs_date}",600,180,0,2,1,1,false)
      label2.draw_text("#{first_line_alt_drugs_date}",600,220,0,2,1,1,false)
