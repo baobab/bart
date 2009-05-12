77db259568576910c0f9378d7b5bc4b94b96607e
Soyapi Mumba <soyapim@gmail.com>
2009-05-12 15:12:17 +0200

    Ignore voided observations when determining outcomes

--- db/migrate/20090511103912_ignore_manually_added_defaulter_outcomes.rb
+++ db/migrate/20090511103912_ignore_manually_added_defaulter_outcomes.rb
@@ -7,7 +7,7 @@ EOF
 
     # Adds AND obs.value_coded <> 373 to WHERE obs.concept_id = 28
 ActiveRecord::Base.connection.execute <<EOF
- CREATE VIEW patient_outcomes (patient_id, outcome_date, outcome_concept_id) AS
+CREATE VIEW patient_outcomes (patient_id, outcome_date, outcome_concept_id) AS
   SELECT encounter.patient_id, encounter.encounter_datetime, 324
   FROM encounter
   INNER JOIN orders ON orders.encounter_id = encounter.encounter_id AND orders.voided = 0
@@ -19,22 +19,22 @@ ActiveRecord::Base.connection.execute <<EOF
   UNION
   SELECT obs.patient_id, obs.obs_datetime, obs.value_coded 
   FROM obs 
-  WHERE obs.concept_id = 28 AND obs.value_coded <> 373
+  WHERE obs.concept_id = 28 AND obs.value_coded <> 373 AND obs.voided = 0
   UNION
   SELECT obs.patient_id, obs.obs_datetime, 325 
   FROM obs 
-  WHERE obs.concept_id = 372 AND obs.value_coded <> 3
+  WHERE obs.concept_id = 372 AND obs.value_coded <> 3 AND obs.voided = 0
   UNION
   SELECT obs.patient_id, obs.obs_datetime, 386 
   FROM obs 
-  WHERE obs.concept_id = 367 AND obs.value_coded <> 3
+  WHERE obs.concept_id = 367 AND obs.value_coded <> 3 AND obs.voided = 0 
   UNION
   SELECT patient_default_dates.patient_id, patient_default_dates.default_date, 373
   FROM patient_default_dates
   UNION
   SELECT patient.patient_id, patient.death_date, 322
   FROM patient
-  WHERE patient.death_date IS NOT NULL;
+  WHERE patient.death_date IS NOT NULL AND patient.voided = 0;
 EOF
 
   end
