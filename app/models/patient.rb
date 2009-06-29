67e8ff2bda8b7fde796dacc41fec8fecb8c48efb
Dave Phiri <davegamphaniphiri@gmail.com>
2009-06-29 12:52:50 +0200

    Changing wording for Presumed HIV disease

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -941,7 +941,7 @@ class Patient < OpenMRS
           presumed_hiv_status_conditions = true
         end
         if age_in_months <= 17 and first_hiv_test_was_rapid and presumed_hiv_status_conditions
-          return Concept.find_by_name("Presumed HIV Status")
+          return Concept.find_by_name("Presumed HIV Disease")
         elsif age_in_months <= 12 and first_hiv_test_was_pcr and hiv_staging != nil #Prevents assigning reason for art b4 staging encounter
           return Concept.find_by_name("PCR Test")
         elsif who_stage >= 3
