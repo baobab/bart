7c97d2fea03c1980487c2a6ba0c27d364d2edafe
Dave Phiri <davegamphaniphiri@gmail.com>
2009-08-07 13:40:31 +0200

    We dont need to modify the structure of patient_start_dates and we need to safely roll back to previous structure of patient_dispensations_and_initiation_dates

--- db/migrate/20090807093120_add_arv_drug_regimen_dispensations.rb
+++ db/migrate/20090807093120_add_arv_drug_regimen_dispensations.rb
@@ -37,25 +37,6 @@ CREATE VIEW patient_dispensations_and_initiation_dates (patient_id, start_date) 
    WHERE concept_id = 143 AND obs.voided = 0;
 EOF
 
-ActiveRecord::Base.connection.execute <<EOF
-DROP VIEW IF EXISTS patient_start_dates;
-EOF
-ActiveRecord::Base.connection.execute <<EOF
-DROP TABLE IF EXISTS patient_start_dates;
-EOF
-
-ActiveRecord::Base.connection.execute <<EOF
-CREATE VIEW patient_start_dates (patient_id, start_date, age_at_initiation) AS
-  SELECT 
-    patient_dispensations_and_initiation_dates.patient_id, 
-    MIN(start_date) AS start_date, 
-    (YEAR(start_date) - YEAR(birthdate)) + IF(((MONTH(start_date) - MONTH(birthdate)) + IF((DAY(start_date) - DAY(birthdate)) < 0, -1, 0)) < 0, -1, 0) +
-    (IF((birthdate_estimated = 1 AND MONTH(birthdate) = 7 AND DAY(birthdate) = 1 AND MONTH(start_date) < MONTH(birthdate)), 1, 0)) AS age_at_initiation 
-  FROM patient_dispensations_and_initiation_dates
-  INNER JOIN patient ON patient.patient_id = patient_dispensations_and_initiation_dates.patient_id
-  GROUP BY patient_dispensations_and_initiation_dates.patient_id;
-EOF
-
   end
 
   def self.down
@@ -68,7 +49,12 @@ DROP VIEW IF EXISTS patient_dispensations_and_initiation_dates;
 EOF
 
 ActiveRecord::Base.connection.execute <<EOF
-DROP VIEW IF EXISTS patient_start_dates;
+CREATE VIEW patient_dispensations_and_initiation_dates (patient_id, start_date) AS
+   SELECT patient_id, dispensed_date AS start_date 
+   FROM patient_first_line_regimen_dispensations
+   UNION SELECT patient_id, value_datetime AS start_date
+   FROM obs
+   WHERE concept_id = 143 AND obs.voided = 0;
 EOF
 
   end
