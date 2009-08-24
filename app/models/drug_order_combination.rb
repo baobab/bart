86ad13032317bbf5cd4a5e629101455db1f3a391
Dave Phiri <davegamphaniphiri@gmail.com>
2009-08-24 09:45:58 +0200

    Getting prescription csv file info into a table

---
+++ app/models/drug_order_combination.rb
@@ -0,0 +1,3 @@
+class DrugOrderCombination < ActiveRecord::Base
+  set_table_name :drug_order_combination
+end
---
+++ app/models/drug_order_combination_regimen.rb
@@ -0,0 +1,3 @@
+class DrugOrderCombinationRegimen < ActiveRecord::Base
+  set_table_name :drug_order_combination_regimen
+end
