753ed8399975bc88b5719ec9d6a2de5ec7c333ed
Dave Phiri <davegamphaniphiri@gmail.com>
2009-08-24 09:54:19 +0200

    Getting prescription csv file info into a table

--- app/models/drug_order_combination.rb
+++ app/models/drug_order_combination.rb
@@ -1,3 +1,4 @@
 class DrugOrderCombination < ActiveRecord::Base
   set_table_name :drug_order_combination
+  belongs_to :drug_order_combination_regimen, :foreign_key => drug_order_combination_regimen_id
 end
