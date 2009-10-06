c4cc480671f0a02fc826bcec7df1c4defae16194
Soyapi Mumba <soyapim@gmail.com>
2009-10-06 10:43:27 +0200

    Fixed migration reverting

--- script/reset_views.rb
+++ script/reset_views.rb
@@ -14,8 +14,14 @@ puts "Reseting views in #{MY_ENV} environment"
 
 if ARGV.include?('--complete')
 
-  puts 'Clearing schema_migrations, sessions, weight_for_heights'
-  ActiveRecord::Base.connection.execute('DELETE FROM schema_migrations;')
+
+
+  puts 'Reverting database migrations'
+  output = `rake db:migrate VERSION=20080405175950 RAILS_ENV=#{MY_ENV}`
+  puts output
+
+  puts 'Clearing sessions, weight_for_heights'
+  #ActiveRecord::Base.connection.execute('DELETE FROM schema_migrations;')
   ActiveRecord::Base.connection.execute('DROP TABLE IF EXISTS sessions;')
   ActiveRecord::Base.connection.execute('DROP TABLE IF EXISTS weight_for_heights;')
 
@@ -46,19 +52,6 @@ PatientHistoricalOutcome.reset
 puts 'Resetting Historical Regimens ....'
 PatientHistoricalRegimen.reset
 
-=begin
-puts 'Ignore outcomes after death date'
-ActiveRecord::Base.connection.execute <<EOF
-DELETE FROM patient_historical_outcomes
-  USING patient_historical_outcomes INNER JOIN 
-  (
-  SELECT * FROM patient_historical_outcomes
-  WHERE outcome_concept_id = 322
-  ORDER BY patient_id, outcome_date, outcome_concept_id
-  ) AS deaths ON patient_historical_outcomes.patient_id = deaths.patient_id
-WHERE deaths.outcome_date < patient_historical_outcomes.outcome_date AND patient_historical_outcomes.outcome_concept_id = 373;
-EOF
-=end
 
 
 
