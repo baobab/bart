5865d9e67830d5da9a7c4e909136f5d0fb14ea5d
mwatha <ace@lalanje.(none)>
2009-11-14 11:53:26 +0200

    adding mcLabeller print functionality - fixing a print bug,was printing few labels

--- app/models/patient_identifier.rb
+++ app/models/patient_identifier.rb
@@ -153,7 +153,7 @@ class PatientIdentifier < OpenMRS
   def self.mclabeller_print(site_code,start_range,end_range,copies)
     print_text = ''
     number_of_loops = end_range 
-    number_of_loops = 1 if number_of_loops == 0
+    number_of_loops = 1 if (end_range - start_range) == 0
     count = start_range
    
     number_of_loops.times{|time|
