c33faa6bfdf38bf5108a3faf080ce56ba74c0244
mwatha <mwatha.bwanali@baobabhealth.org>
2010-08-12 10:38:54 -0400

    moving 'after_save method' to orders and obs models

--- app/models/encounter.rb
+++ app/models/encounter.rb
@@ -567,28 +567,6 @@ EOF
 =end
   end
 
-  def after_save
-
-    encounter_patient = self.patient
-    encounter_name = self.name
-
-    if encounter_name == "Give drugs"
-      encounter_patient.reset_regimens
-      encounter_patient.reset_outcomes
-      encounter_patient.reset_adherence_rates
-      if encounter_patient.date_started_art 
-        encounter_patient.reset_start_date if encounter_patient.date_started_art > self.encounter_datetime
-      else
-        encounter_patient.reset_start_date
-      end
-    elsif encounter_name == "HIV First visit" #TODO Do this only if transfer in(ie patient has date_of_art_initiation observation) 
-      encounter_patient.reset_start_date
-    elsif encounter_name == "ART Visit" or encounter_name == "Update outcome" #TODO  
-      encounter_patient.reset_outcomes
-      encounter_patient.reset_adherence_rates
-    end
-  end
-
   def hiv_stage(observation_date = Date.today)
     return nil if self.name != "HIV Staging"
       yes_concept = Concept.find_by_name "Yes"
