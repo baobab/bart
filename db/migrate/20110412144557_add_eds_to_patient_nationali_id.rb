9c5457b67f35ccf340a5078c6e0b1c499eef9070
mwatha <mwatha.bwanalie@baobabhealth.org>
2011-04-12 16:58:06 +0200

    adding a new column in patient_national_id table

--- db/migrate/20110412144557_add_eds_to_patient_nationali_id.rb
+++ db/migrate/20110412144557_add_eds_to_patient_nationali_id.rb
@@ -1,7 +1,6 @@
 class AddEdsToPatientNationaliId < ActiveRecord::Migration
   def self.up
-    add_column :patient_nationali_id, :eds, :boolen
-    add_column :patient_nationali_id, :default, :0
+    add_column :patient_nationali_id, :eds, :boolen, :default => 0
   end
 
   def self.down
