1eb509737371c74249b0519e9a7c033db8a1ce6d
gamphani <dave.phiri@baobabhealth.org>
2011-10-11 04:32:25 +0200

    Mapped new cohort report fields with actual concepts

--- app/models/reports/cohort_by_registration_date.rb
+++ app/models/reports/cohort_by_registration_date.rb
@@ -1096,6 +1096,9 @@ class Reports::CohortByRegistrationDate
     cohort_values['who_stage_2_lymphocyte'] = start_reasons[0]["Lymphocyte count below threshold with WHO stage 2"]
     cohort_values['infants_PCR'] = start_reasons[0]["PCR Test"]
     cohort_values['infants_presumed_severe_HIV'] = start_reasons[0]["Presumed HIV Disease"]
+    cohort_values['child_hiv_positive'] = start_reasons[0]["Child HIV positive"]
+    cohort_values['breastfeeding_mothers'] = start_reasons[0]["Breastfeeding"]
+    cohort_values['pregnant_women'] = start_reasons[0]["Pregnant"]
     cohort_values['who_stage_3'] = start_reasons[0]["WHO stage 3"] || start_reasons[0]["WHO Stage 3"] || start_reasons[0][" Stage 3"] || 0
     cohort_values['who_stage_4'] = start_reasons[0]["WHO stage 4"] || start_reasons[0]["WHO Stage 4"] || start_reasons[0][" Stage 4"] || 0
     cohort_values['start_reason_other'] = start_reasons[0]["Other"] || 0
@@ -1280,6 +1283,9 @@ class Reports::CohortByRegistrationDate
      'child_patients' => 'children_started_on_arv_therapy',
      'infant_patients' => 'infants_started_on_arv_therapy',
      'infants_presumed_severe_HIV' => 'patients_with_start_reason,Presumed HIV Disease',
+     'child_hiv_positive' => 'patients_with_start_reason,child_hiv_positive',
+     'breastfeeding_mothers' => 'patients_with_start_reason,breastfeeding_mothers',
+     'pregnant_women' => 'patients_with_start_reason,pregnant_women',
      'infants_PCR' => 'patients_with_start_reason,PCR Test',
      'who_stage_1_or_2_cd4' => 'patients_with_start_reason,who_stage_1_or_2_cd4',
      'who_stage_2_lymphocyte' => 'patients_with_start_reason,Lymphocyte count below threshold with WHO stage 2',
