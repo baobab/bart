9a0073a3fe6d350602dec6773a186436b2598349
timmtonga <timmtonga@gmail.com>
2014-02-21 12:08:01 +0200

    Added two main functions and validate presence of start reason

--- app/models/validation_rule.rb
+++ app/models/validation_rule.rb
@@ -1,2 +1,16 @@
 class ValidationRule < ActiveRecord::Base
+
+  def self.data_consistency_checks(date = Date.today)
+
+  end
+
+  def self.create_update_validation_result(rule, date, result)
+
+  end
+
+  def self.validate_presence_of_start_reason
+    #This function checks for patients who do not have a reason for starting ART
+
+
+  end
 end
